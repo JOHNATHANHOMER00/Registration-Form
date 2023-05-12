1fe98a6a08dd2aa4c84359730aaf2cd7ae673fa3
Johnathan Homer <61136223+JOHNATHANHOMER00@users.noreply.github.com>
2023-05-12 23:14:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,46 +28,44 @@ with st.form(key='personal'):
 
     dob = st.date_input(label='Date of Birth', min_value=(datetime.date(1900, 1, 1)))
     splitdate = str(dob).split('-')
-    if int(splitdate[0]) >= year - 10:
-        st.write('We have some specials events for the under 10\'s | *age will be take to the year*')
+    
 
-        st.session_state['count'] = 0
+    st.session_state['count'] = 0
 
-        st.write("Select Events")
-        st.write('*All events are subject to a split in class of A & B depending on the number of entries and timings*')
-        st.write("Rs/: 1500 per event")
-        ev1 = st.checkbox("1/10 Onroad (will be split into A / B depending on entry count and timings)")
-        ev2 = st.checkbox("1/8 Nitro Buggy")
-        ev3 = st.checkbox("1/8 Nitro Truggy")
-        ev4 = st.checkbox("1/8 Electric Buggy")
-        ev5 = st.checkbox("1/8 Electric Truggy")
+    st.write("Select Events")
+    st.write('*All events are subject to a split in class of A & B depending on the number of entries and timings*')
+    st.write("Rs/: 1500 per event")
+    ev1 = st.checkbox("1/10 Onroad (will be split into A / B depending on entry count and timings)")
+    ev2 = st.checkbox("1/8 Nitro Buggy")
+    ev3 = st.checkbox("1/8 Nitro Truggy")
+    ev4 = st.checkbox("1/8 Electric Buggy")
+    ev5 = st.checkbox("1/8 Electric Truggy")
 
-        if int(splitdate[0]) >= year - 10:
-            st.write('Events for under 10\'s')
-            ev6 = st.checkbox("1/10 Onroad Junior (10 and Under)")
-            ev7 = st.checkbox("1/8 & 1/10 Offroad Junior Open (10 and Under)")
+    st.write('Events for under 10\'s')
+    ev6 = st.checkbox("1/10 Onroad Junior (10 and Under)")
+    ev7 = st.checkbox("1/8 & 1/10 Offroad Junior Open (10 and Under)")
 
-        submit_button = st.form_submit_button('register')
+    submit_button = st.form_submit_button('register')
 
 
-        if submit_button:
-            if ev1:
-                st.session_state['count'] += 1
-            if ev2:
-                st.session_state['count'] += 1
-            if ev3:
-                st.session_state['count'] += 1
-            if ev4:
-                st.session_state['count'] += 1
-            if ev5:
-                st.session_state['count'] += 1
-            if ev6:
-                st.session_state['count'] += 1
-            if ev7:
-                st.session_state['count'] += 1
+    if submit_button:
+        if ev1:
+            st.session_state['count'] += 1
+        if ev2:
+            st.session_state['count'] += 1
+        if ev3:
+            st.session_state['count'] += 1
+        if ev4:
+            st.session_state['count'] += 1
+        if ev5:
+            st.session_state['count'] += 1
+        if ev6:
+            st.session_state['count'] += 1
+        if ev7:
+            st.session_state['count'] += 1
 
-            st.write(st.session_state['count'])
-            with st.expander("Payment Details", expanded=True):
-                st.write("Bank : ")
-                value = st.session_state['count'] * 1500
-                st.write(value)
+        st.write(st.session_state['count'])
+        with st.expander("Payment Details", expanded=True):
+            st.write("Bank : ")
+            value = st.session_state['count'] * 1500
+            st.write(value)
